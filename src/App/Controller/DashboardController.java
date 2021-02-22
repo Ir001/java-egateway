229c2d3fd81ad32a0fa1d753e36809a2d8a108d6
Irwan Antonio <irwanantonio2708@gmail.com>
2021-02-22 16:07:30 +0700

    Logout

--- src/App/Controller/DashboardController.java
+++ src/App/Controller/DashboardController.java
@@ -2,7 +2,6 @@ package App.Controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
